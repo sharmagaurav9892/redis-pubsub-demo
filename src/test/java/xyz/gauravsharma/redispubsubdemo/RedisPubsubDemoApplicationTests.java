a33972916de326d8f6608c8f5475cae6c825834a
gauravsharma <gaurav.sharma@rksv.in>
2023-06-27 22:01:50 +0530

    Initial Commit.

---
+++ src/test/java/xyz/gauravsharma/redispubsubdemo/RedisPubsubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gauravsharma.redispubsubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPubsubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
