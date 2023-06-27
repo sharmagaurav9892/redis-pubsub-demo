ccf024a3a4dfd8a9860b46fea9043be06fbed69f
gauravsharma <gaurav.sharma@rksv.in>
2023-06-27 22:07:01 +0530

    Initial Commit.

--- src/main/java/xyz/gauravsharma/redispubsubdemo/RedisPubsubDemoApplication.java
+++ src/main/java/xyz/gauravsharma/redispubsubdemo/RedisPubsubDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisPubsubDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RedisPubsubDemoApplication.class, args);
 	}
 
